739d593bc09b7d09d7e249c1f5300a6b036af562
gabrieleciriano <cirianogabriele@gmail.com>
2023-03-10 20:11:36 -0300

    add: metodos set na classe produto e alterado a program

--- Aula_05/Program.cs
+++ Aula_05/Program.cs
@@ -3,3 +3,8 @@
 Produto p1 = new Produto(1, 10.90M, "Cerveja");
 
 Console.WriteLine(p1.getId()+" "+p1.getDescricao());
+
+p1.setValor(11.85M);
+p1.setDescricao("Cerveja Skol 1 litrão");
+
+Console.WriteLine(p1.getId() + " " + p1.getDescricao());
