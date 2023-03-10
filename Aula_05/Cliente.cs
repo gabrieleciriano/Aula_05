217730eb943c845dc8ae7629abbebe5d2c42eea8
gabrieleciriano <cirianogabriele@gmail.com>
2023-03-10 20:24:11 -0300

    add: classe cliente e seus metodos get e set

---
+++ Aula_05/Cliente.cs
@@ -0,0 +1,48 @@
+﻿namespace Aula_05
+{
+    public sealed class Cliente
+    {
+        public Cliente(int id, string nome, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+
+        }
+
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+       
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public void setNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone = telefone;
+        }
+    }
+}
