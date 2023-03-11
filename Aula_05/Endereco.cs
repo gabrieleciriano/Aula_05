89d95c30e7f48db62520744d9c3b73b41986d11c
gabrieleciriano <cirianogabriele@gmail.com>
2023-03-10 21:27:01 -0300

    add: classe endereco e os metodos get e set

---
+++ Aula_05/Endereco.cs
@@ -0,0 +1,81 @@
+﻿namespace Aula_05
+{
+    public sealed class Endereco
+    {
+        private string Rua;
+        private string Numero;
+        private string Bairro;
+        private string Cep;
+        private string Cidade;
+        private string Estado;
+
+        public Endereco(string rua, string numero, string bairro, string cep, string cidade, string estado)
+        {
+            Rua = rua;
+            Numero = numero;
+            Bairro = bairro;
+            Cep = cep;
+            Cidade = cidade;
+            Estado = estado;
+        }
+
+        public string getRua()
+        {
+            return Rua;
+        }
+
+        public void setRua(string rua)
+        {
+            Rua = rua;
+        }
+
+        public string getNumero() 
+        { 
+            return Numero;
+        }
+
+        public void setNumero(string numero)
+        {
+            Numero = numero;
+        }
+
+        public string getBairro()
+        {
+            return Bairro;
+        }
+
+        public void setBairro(string bairro)
+        {
+             Bairro = bairro;
+        }
+
+        public string getCep()
+        {
+            return Cep;
+        }
+        public void setCep(string cep)
+        {
+            Cep = cep;
+        }
+
+        public string getCidade()
+        {
+            return Cidade;
+        }
+
+        public void setCidade(string cidade)
+        {
+            Cidade = cidade;
+        }
+
+        public string getEstado()
+        {
+            return Estado;
+        }
+
+        public void setEstado(string estado)
+        {
+            Estado = estado;
+        }
+    }
+}
